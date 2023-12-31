37c3534ee59c702ee44167d9dc12be1560fcc945
Jatin Chandrakant Parmar <jatinparmar06@gmail.com>
2023-12-31 11:36:47 -0500

    Update CommentRepository.java
    Changed in the SQL query.

--- src/main/java/com/neublog/neublogwebapp/repository/CommentRepository.java
+++ src/main/java/com/neublog/neublogwebapp/repository/CommentRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment,Integer> {
 
-    @Query(value = "select c.* from comments c inner join posts\n" +
+    @Query(value = "select c.* from comments c inner join posts p\n" +
             "where c.post_id = p.id and p.created_by=:userId", nativeQuery = true)
     List<Comment> findCommentsByPost(Integer userId);
 }
